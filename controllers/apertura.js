af30abf4048b9a35c6bac101b9c2bc5b0caf541f
Eder <vk62001@gmail.com>
2023-02-23 02:26:38 -0600

    add apertura tienda

--- controllers/apertura.js
+++ controllers/apertura.js
@@ -6,36 +6,47 @@ const URILocal = process.env.URi_local;
 
 const apertura = async (req, res) => {
 
-    const {code, id} = req.body
-    console.log(code, id)
-    if(id === undefined){
-      res.status(200).send({ok:true});
-      return;
-    }
-    //consultar base local
-    
+  const {code, id} = req.body;
+  console.log(code, id);
+  if(id === undefined){
+    res.status(200).send({ok:true});
+    return;
+  };
 
-    //post
-    const config = {
-      method: 'post',
-      url: `${URICentral}api/v1/AperturasTiendas/`,
-      headers: { 
-        'Authorization': 'Basic ZnRwc2FwOkluaWNpbzAx', 
-        'Content-Type': 'application/json'
-      },
-    };
-        setTimeout(()=>{
-          axios(config)
-          .then(function (response) {
-          console.log(JSON.stringify(response.data, '--'));
-            console.log('log salvado')
-          })
-          .catch(function (error) {
-          console.log(error.data, 'errado');
-          res.status(500)
-      });
-    }, 100);
-    res.status(200).send({data:true});
+  setTimeout(async ()=>{
+
+  const objData =  await axios.get(`${URILocal}api/v1/AperturasTiendas/${id}`)
+                  .then(res=>{
+                    return res.data.datas
+                  })
+                  .catch(err=>console.log(err));
+
+  console.log(objData, '-')
+  const config = {
+    method: 'post',
+    url: `${URICentral}api/v1/AperturasTiendas/`,
+    headers: { 
+      'Authorization': 'Basic ZnRwc2FwOkluaWNpbzAx', 
+      'Content-Type': 'application/json'
+    },
+    data:objData[0]
+  };
+
+  setTimeout(()=>{
+    axios(config)
+    .then(function (response) {
+        console.log(JSON.stringify(response.data, '--'));
+        console.log('log salvado');
+        
+    })
+    .catch(function (error) {
+        console.log(error, 'errado');
+        // res.status(200).send({ok:true})
+    });
+  }, 200);
+}, 500)
+
+  res.status(200).send({ok:true});
 };
 
 
