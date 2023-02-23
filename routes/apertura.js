c225d20e3b7fb511e6d58aa26dc2fe5fbab43950
Eder <vk62001@gmail.com>
2023-02-23 02:34:01 -0600

    clean route apertura

--- routes/apertura.js
+++ routes/apertura.js
@@ -2,18 +2,8 @@ const { apertura, updateApertura } = require('../controllers/apertura');
 
 const router = require('express').Router();
 
-
-router.get('/', (req, res)=>{
-    res.send('hola mundo')
-});
-
 router.post('/', apertura);
 
 router.put('/', updateApertura);
-
-router.delete('/', (req, res)=>{
-    res.send({resp:'delete'});
-});
-
 
 module.exports = router
